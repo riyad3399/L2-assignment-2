a3fc7cd4d3fc8e1b8a4e23c9b31bad8d4abe6e91
riyad3399 <riyad3399>
2023-11-29 23:15:03 +0600

    added the interface file

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,27 @@
+export type TOrders = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type TAddress = {
+  street: string;
+  city: string;
+  country: string;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: Array<string>;
+  address: TAddress;
+  orders: TOrders[];
+};
